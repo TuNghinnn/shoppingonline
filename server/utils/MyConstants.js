83b3263153f8ec9422d13e1004c7131a006bf209
Nguyen Dat <dat212923@gmail.com>
2024-07-16 16:03:36 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.gcf4xjl.mongodb.net',
+  DB_USER: 'dat',
+  DB_PASS: 'datdat',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'khoa_bimat',
+  JWT_EXPIRES: '600000000', // in milliseconds
+  EMAIL_USER: 'dat212923@gmail.com', // gmail service
+  EMAIL_PASS: 'fqkl njpy dhvx anha'
+};
+module.exports = MyConstants;
